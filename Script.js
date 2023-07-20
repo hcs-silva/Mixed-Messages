1c8c2698dd0670e62e3d980de6738ca045508dca
Hernâni Silva <hcsilvamatos@hotmail.com>
2023-07-20 11:48:43 +0100

    add switch Statement

--- Script.js
+++ Script.js
@@ -4,14 +4,27 @@ let randNum = (num) =>{
     return Math.floor(Math.random()*num);
 }
 
-console.log(randNum(3))
+
 //Add an object with the arrays containing the Messages
 const Motivation= {
     Emotion: ["Sad","Angry","Frustrated","Happy","Embarassed" ],
-    
-}
+    Quote: ["Keep Kalm", "Enjoy The Ride","Keep Dreaming","Play Dead","Keep Fighting"],
+    Solution: ["Push Harder","Pay Attention","Look Around", "You are not alone"]
 
+}
+let Motivational =[];
 //iterate through the object
-for (let emotion in Motivation) {
+for (let property in Motivation) {
+switch (property) {
+    case "Emotion":
+        let emotionIndex =randNum(Motivation[property].length);
+        Motivational.push(Motivation[property][emotionIndex]);
+    case "Quote" :
+        let quoteIndex =randNum(Motivation[property].length);
+        Motivational.push(Motivation[property][qouteIndex]);
+    case "Solution" :
+        let solutionIndex =randNum(Motivation[property].length);
+        Motivational.push(Motivation[property][solutionIndex]);
+}
 
 }
